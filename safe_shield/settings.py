2b6fed47c101ef8e42d329a0b51e3f34ba4b80f7
R.V <teytdduydg@gmail.com>
2026-07-09 09:30:37 +0300

    Fix allowed hosts

--- safe_shield/settings.py
+++ safe_shield/settings.py
@@ -25,11 +25,8 @@ SECRET_KEY = 'django-insecure-!n89pzk!4iz2-i3hdm4q&y$b*^%fsye2nhu^!^ybv0k8q@p_h4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "192.168.1.164",
-]
+ALLOWED_HOSTS = ['safeshield-v1db.onrender.com']
+
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
